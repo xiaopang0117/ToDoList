70061a64c2b11531cbbef4d1a6c05634b7fa62db
jiangkuan <jiangkuan@lixiang.com>
2020-07-07 22:34:54 +0800

    todo list

---
+++ src/TodoList.js
@@ -0,0 +1,38 @@
+import React, { Component, Fragment } from "react";
+
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button>提交</button>
+        </div>
+        <ul>
+          <li>学英语</li>
+          <li>学日语</li>
+        </ul>
+      </Fragment>
+    )
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+}
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// App组件,大写字母开头都是组件
+import TodoList from "./TodoList";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<TodoList />, document.getElementById("root"));
